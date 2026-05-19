ebb4818acfcc4e16e93bce9b01855c9d5cbfd740
femarko <fenstermark7@gmail.com>
2026-05-19 16:25:32 +0200

    refactor(infrastructure): move department-specific queries from base repository to DepartmentRepo
    - remove find_by_name_and_parent_id from BaseRepository to DepartmentRepo
    - add get_by_parent_id for hierarchy traversal to DepartmentRepo and impementation
    - refactor find_by_name_and_parent_id to use explicit SQLAlchemy exception handling

--- src/org_struct/infrastructure/db/repositories.py
+++ src/org_struct/infrastructure/db/repositories.py
@@ -1,7 +1,9 @@
 from typing import Generic
 from datetime import datetime
 from sqlalchemy.orm import selectinload
+from sqlalchemy.exc import NoResultFound as SQLAlchemyNoResultFound
 
+from org_struct.domain.errors import DepartmentNotFound
 from org_struct.domain.models import (
     T_Model,
     T_Department,
@@ -27,18 +29,6 @@ class BaseRepository(Generic[T_Model]):
 
     def get_by_id(self, model_id: int) -> T_Model | None:
         return self.session.get(self.model_cls, model_id)
-    
-    def find_by_name_and_parent_id(
-            self,
-            name: str,
-            parent_id: int
-    ) -> int | None:
-        dep_fetched = self.session.query(self.model_cls).filter_by(
-            name=name,
-            parent_id=parent_id
-        ).one()
-        result = dep_fetched.id if dep_fetched else None
-        return result
 
     def delete(self, model: T_Model) -> None:
         self.session.delete(model)
@@ -55,5 +45,39 @@ class DepartmentRepo(BaseRepository[T_Department]):
             .filter(self.model_cls.id == department_id)
             .one()
         )
+    
+    def get_by_parent_id(self, parent_id: int) -> T_Department | None:
+        try:
+            result = (
+                self.session.query(self.model_cls)
+                .filter(self.model_cls.parent_id == parent_id)
+                .one()
+            )
+        except SQLAlchemyNoResultFound as e:
+            raise DepartmentNotFound(
+                f"Department with {parent_id=} does not exist"
+            ) from e
+        else:
+            return result
+
+    def find_by_name_and_parent_id(
+            self,
+            name: str,
+            parent_id: int
+    ) -> int | None:
+        try:
+            dep_fetched = self.session.query(self.model_cls).filter_by(
+                name=name,
+                parent_id=parent_id
+            ).one()
+        except SQLAlchemyNoResultFound as e:
+            raise DepartmentNotFound(
+                f"Department with {name=} name and {parent_id=} "
+                f"does not exist"
+            ) from e
+        else:
+            result = dep_fetched.id if dep_fetched else None
+            return result
+
 
 class EmployeeRepo(BaseRepository[T_Employee]): ...
